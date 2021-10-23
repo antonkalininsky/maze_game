e9fb074a9b3bcd03976a14b345a7b9e86656d5d9
antonkalininsky <chadik.free@gmail.com>
2021-10-23 10:26:06 +0700

    class Board: start of maze drawing implementation

--- board.cpp
+++ board.cpp
@@ -3,6 +3,7 @@
 #include <QGraphicsTextItem>
 #include <QString>
 #include <QPen>
+#include <QGraphicsLineItem>
 
 
 Board::Board(int rectSz, int mapSz, int **map) {
@@ -10,6 +11,12 @@ Board::Board(int rectSz, int mapSz, int **map) {
     QPen pen;
     pen.setColor(Qt::gray);
     pen.setStyle(Qt::DotLine);
+
+
+    QPen pen2;
+    pen2.setColor(Qt::black);
+    pen2.setStyle(Qt::SolidLine);
+    pen2.setWidth(3);
 
     // draw squares n numbers
     for (int i = 0; i < mapSz; i++) {
@@ -23,6 +30,36 @@ Board::Board(int rectSz, int mapSz, int **map) {
             tile->setPen(pen);
             addItem(tile);
             addItem(num);
+        }
+    }
+
+    // borders
+    QGraphicsLineItem* line = new QGraphicsLineItem();
+    line->setLine(0, 0, 0, mapSz*rectSz);
+    line->setPen(pen2);
+    addItem(line);
+    line = new QGraphicsLineItem();
+    line->setLine(0, 0, mapSz*rectSz, 0);
+    line->setPen(pen2);
+    addItem(line);
+    line = new QGraphicsLineItem();
+    line->setLine(0, mapSz*rectSz, mapSz*rectSz, mapSz*rectSz);
+    line->setPen(pen2);
+    addItem(line);
+    line = new QGraphicsLineItem();
+    line->setLine(mapSz*rectSz, 0, mapSz*rectSz, mapSz*rectSz);
+    line->setPen(pen2);
+    addItem(line);
+
+    // maze
+    for (int i = 0; i < mapSz; i++) {
+        for (int l = 0; l < mapSz; l++) {
+            line = new QGraphicsLineItem();
+            line->setPen(pen2);
+            if (map[i][l] >= map[i][l+1]) {
+                line->setLine(i*rectSz, rectSz*(1+l), rectSz, rectSz*(1+l));
+                addItem(line);
+            }
         }
     }
 
